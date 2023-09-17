175806dc631ecbaa66717ef7a486ea34bb76877c
Michael Girma <michaelgirma@wireless-10-105-33-179.umd.edu>
2023-09-16 20:43:00 -0400

    finished steps 0 to 4

---
+++ server/data/gifts.js
@@ -0,0 +1,44 @@
+const giftData = [
+    {
+      "id": 1,
+      "name": "Essential Shorts",
+      "pricePoint": "$55",
+      "audience": "Fashionable Shorts",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjhZdwolwZtyNEHJFBuQwp6B2-A_c2HGQtDQ&usqp=CAU",
+      "description": "Shorts designed by Kanye",
+      "submittedBy": "Michael",
+      "submittedOn": "2022-09-04T14:48:00"
+    },
+    {
+      "id": 2,
+      "name": "Gamer Headset Set",
+      "pricePoint": "$35",
+      "audience": "Video Game lovers",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsWrOBVAm4yas8Xtv7p6Pow1i1xuBRGNX8Kg&usqp=CAU",
+      "description": "Best quality audio for your gaming needs",
+      "submittedBy": "Chris",
+      "submittedOn": "2022-08-15T11:37:00"
+    },
+    {
+      "id": 3,
+      "name": "Porsche Key Chain",
+      "pricePoint": "$15",
+      "audience": "Porsche Lovers",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKMPSbSVUNuC0uaLBg6LtFaRcA3LEIPwMEjQ&usqp=CAU",
+      "description": "Get a keycahin to match your porsche.",
+      "submittedBy": "Jane",
+      "submittedOn": "2022-09-10T17:22:00"
+    },
+    {
+      "id": 4,
+      "name": "Iphone15",
+      "pricePoint": "$1000",
+      "audience": "Apple Product Lovers",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHDBAXaGteRw47g2AhYhrIM5Z_l1fz2SU9pQ&usqp=CAU",
+      "description": "Latest Apple Iphone Model",
+      "submittedBy": "Cristina",
+      "submittedOn": "2022-08-24T13:45:00"
+    }
+  ];
+  
+  export default giftData;
